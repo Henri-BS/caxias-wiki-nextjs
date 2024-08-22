66312269c2d9b5e6b47fd58b12edb4e65cf1a914
Henrique Santos <rickalmeida720@gmail.com>
2024-08-21 21:15:20 -0300

    backend config

---
+++ backend/src/main/java/com/pasifcode/caxias_destiny/CaxiasDestinyApplication.java
@@ -0,0 +1,13 @@
+package com.pasifcode.caxias_destiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaxiasDestinyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaxiasDestinyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pasifcode/caxias_destiny/CaxiasDestinyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasifcode.caxias_destiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaxiasDestinyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
