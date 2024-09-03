aad3ba393961f2617ba37a550a953bb9a73f5db1
Henrique Santos <rickalmeida720@gmail.com>
2024-09-03 06:36:32 -0300

    Add StorySpecs

--- backend/src/main/java/com/pasifcode/caxias_destiny/infra/repository/StoryRepository.java
+++ backend/src/main/java/com/pasifcode/caxias_destiny/infra/repository/StoryRepository.java
@@ -8,18 +8,16 @@ import org.springframework.util.StringUtils;
 
 import java.util.List;
 
+import static com.pasifcode.caxias_destiny.infra.spec.GenericSpec.*;
+import static com.pasifcode.caxias_destiny.infra.spec.StorySpec.*;
+import static org.springframework.data.jpa.domain.Specification.where;
+
 public interface StoryRepository extends JpaRepository<Story, String>, JpaSpecificationExecutor<Story> {
     default List<Story> findByNameOrLocationsOrTagsLike(String query) {
-        Specification<Story> conjuction = (root, q, criteriaBuilder) -> criteriaBuilder.conjunction();
-        Specification<Story> spec = Specification.where(conjuction);
+        Specification<Story> spec = where(conjuction());
 
         if (StringUtils.hasText(query)) {
-            Specification<Story> nameLike = (root, q, cb) -> cb.like(cb.upper(root.get("name")), "%" + query.toUpperCase() + "%");
-            Specification<Story> locationsLike = (root, q, cb) -> cb.like(cb.upper(root.get("locations")), "%" + query.toUpperCase() + "%");
-            Specification<Story> tagsLike = (root, q, cb) -> cb.like(cb.upper(root.get("tags")), "%" + query.toUpperCase() + "%");
-
-            Specification<Story> nameOrLocationsOrTagsLike = Specification.anyOf(nameLike, locationsLike, tagsLike);
-            spec = spec.and(nameOrLocationsOrTagsLike);
+            spec = spec.and(Specification.anyOf(nameLike(query), locationsLike(query), tagsLike(query)));
         }
         return findAll(spec);
     }
---
+++ backend/src/main/java/com/pasifcode/caxias_destiny/infra/spec/StorySpec.java
@@ -0,0 +1,21 @@
+package com.pasifcode.caxias_destiny.infra.spec;
+
+import com.pasifcode.caxias_destiny.domain.entity.Story;
+import org.springframework.data.jpa.domain.Specification;
+
+public class StorySpec {
+
+    private StorySpec(){}
+
+    public static Specification<Story> nameLike(String name) {
+        return (root, q, cb) -> cb.like(cb.upper(root.get("name")), "%" + name.toUpperCase() + "%");
+    }
+
+    public static Specification<Story> locationsLike(String locations) {
+        return (root, q, cb) -> cb.like(cb.upper(root.get("locations")), "%" + locations.toUpperCase() + "%");
+    }
+
+    public static Specification<Story> tagsLike(String tags) {
+        return (root, q, cb) -> cb.like(cb.upper(root.get("tags")), "%" + tags.toUpperCase() + "%");
+    }
+}
