bb60abfbab78b8ae9df4fcd549830bd4e202d9c8
Henrique Santos <rickalmeida720@gmail.com>
2024-09-08 22:47:10 -0300

    Add generateToken configs (backend)

--- backend/src/main/java/com/pasifcode/caxias_destiny/service/impl/JwtService.java
+++ backend/src/main/java/com/pasifcode/caxias_destiny/service/impl/JwtService.java
@@ -1,12 +1,51 @@
 package com.pasifcode.caxias_destiny.service.impl;
 
+import com.pasifcode.caxias_destiny.config.SecretKeyGeneratorConfig;
 import com.pasifcode.caxias_destiny.domain.AccessToken;
 import com.pasifcode.caxias_destiny.domain.entity.User;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
+@RequiredArgsConstructor
 public class JwtService {
+
+    private final SecretKeyGeneratorConfig keyGenerator;
+
     public AccessToken generateToken(User user) {
+
+        SecretKey key = keyGenerator.getKey();
+        Date expirationDate = generateExpirationDate();
+        Map<String, Object> claims = generateTokenClaims(user);
+
+        String token = Jwts
+                .builder()
+                .signWith(key)
+                .subject(user.getEmail())
+                .expiration(expirationDate)
+                .claims(claims)
+                .compact();
         return new AccessToken("");
+    }
+
+
+    private Date generateExpirationDate() {
+        var expirationMinutes = 60;
+        LocalDateTime now = LocalDateTime.now().plusMinutes(expirationMinutes);
+        return Date.from(now.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    private Map<String, Object> generateTokenClaims(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("name", user.getName());
+        return claims;
     }
 }
